061c61355bded08d6f012bacb513bb984c699d76
Sanjeev Kumar <kumar.san96@gmail.com>
2022-09-10 21:30:54 +0200

    adding tbot3 pkg

---
+++ tbot3/src/landmark_client.cpp
@@ -0,0 +1,67 @@
+/* ---------------ROS CLIENT------------------
+
+___Important_Points___
+## will printout the closest landmark
+## print out the distance of each landmark
+## don't need to use the ros::spin(), bcz landmark_client just prints
+   the data and then stop 
+## wait for duration is used to synchronize the client and the server:
+    useful when sometime client execute before the server
+*/
+#include <string>
+#include <vector>
+#include <iostream>
+
+#include "ros/ros.h"
+#include "tbot3_msgs/GetClosest.h"
+#include "tbot3_msgs/GetDistance.h"
+#include "tbot3_msgs/GetClosestRequest.h"
+
+
+int main(int argc, char**argv){
+    // initialize the ros node with name "landmark_client"
+    ros::init(argc, argv, "landmark_client");
+    ros::NodeHandle nh;
+
+    //only added to make the ros::waitForExistence to work
+    while(ros::Time::now().isZero()){
+        ros::spinOnce();
+    }
+
+    //create the service client and hook it with the available service of the node here "get_closest"
+    ros::ServiceClient get_closest = nh.serviceClient<tbot3_msgs::GetClosest>("get_closest");
+    ros::ServiceClient get_distance = nh.serviceClient<tbot3_msgs::GetDistance>("get_distance");
+
+    //implement the waitForExistence to synchronize the client and server
+    if (!get_closest.waitForExistence(ros::Duration(5))){
+        ROS_ERROR("get_closest server did not come up in 5 seconds!");
+        return 1;
+    }
+
+    if(!get_distance.waitForExistence(ros::Duration(5))){
+        ROS_ERROR("get_distance server did not come up in 5 seconds");
+        return 1;
+    }
+    //call the service via client
+    tbot3_msgs::GetClosestRequest req;
+    tbot3_msgs::GetClosestResponse res;
+    if (!get_closest.call(req, res)){
+        ROS_ERROR("Failed to call GetClosest!");
+    }
+
+    std::cout<< "Closest Landmark: "<<res.name<<std::endl;
+    //for closest distance create vector of landmarks
+    std::vector<std::string> landmarks_list = {"Construction_barrel","Dumpster", "Number1", "Jersey_barrier"};
+
+    for(const std::string &name: landmarks_list){
+        tbot3_msgs::GetDistanceRequest dist_req;
+        dist_req.name = name;
+        tbot3_msgs::GetDistanceResponse dist_res;
+        get_distance.call(dist_req, dist_res);
+        std::cout<<name<<" : "<<dist_res.distance<<std::endl;
+
+    }
+    return 0;
+
+}
+
---
+++ tbot3/src/landmark_monitor.cpp
@@ -0,0 +1,222 @@
+// #include "ros/ros.h"
+// #include "tbot3_msgs/GetClosest.h"
+// #include "tbot3_msgs/GetDistance.h"
+// // #include "nav_msgs/Odometry.h"
+// #include<map>
+// struct Landmark
+// {
+//   /* landmark list attributes */
+//   std::string name;
+//   double x;
+//   double y;
+// };
+
+
+// class LandmarkMonitor {
+// private:
+//   /* attributes */
+// //   std::map<std::string, Landmark> landmarks_list;
+// //   double robot_x;          // to get these values listen odom topic
+// //   double robot_y;          // for this use the subscribe functionality of ros node
+
+// public:
+//   /* class constructors & destrcutors */
+//   LandmarkMonitor();
+// //   LandmarkMonitor():
+// //     landmarks_list(), robot_x{0}, robot_y{0} {
+// //         landmarks_list = { 
+// //             {"Construction_barrel", {"Constrcution_barrel", 5.20314, 3.591}},
+// //             {"Dumpster", {"Dumpster", 3.190, -4.9259}},
+// //             {"Jersey_barrier", {"Jersey_barrier", 0.31978, 1.38024}},
+// //             {"Number1", {"Number1", 0.359466, 3.82409}}
+// //         };
+// //     }
+//   ~LandmarkMonitor();
+
+//   /* class methods to implement service callback */
+//   bool GetClosest_callback(tbot3_msgs::GetClosest::Request& request, tbot3_msgs::GetClosest::Response& response){
+//     ROS_INFO("GetClosest Called");
+//     return true;
+//   }
+
+//   bool GetDistance_callback(tbot3_msgs::GetDistance::Request& request, tbot3_msgs::GetDistance::Response& response){
+//     ROS_INFO("GetDistance Called for %s", request.name.c_str());
+
+//     //check if the landmark exit or not
+//     // if(landmarks_list.find(request.name) == landmarks_list.end()){
+//     //     ROS_ERROR("Unknown Landmarks [%s]", request.name.c_str());
+//     // }
+//     return true;
+//   }
+  
+//   /* class methods to implement subscribe callback
+//    * created to get the x and y position of the robot from the odom topic
+//   */
+// //   void Odom_callback(const nav_msgs::OdometryConstPtr& msg){
+// //     robot_x = msg->pose.pose.position.x;
+// //     robot_y = msg->pose.pose.position.y;
+// //   }
+
+// };
+
+// LandmarkMonitor ::LandmarkMonitor(/* args */)
+// {
+// }
+
+// LandmarkMonitor ::~LandmarkMonitor()
+// {
+// }
+
+// int main(int argc, char** argv)
+// {
+//   ros::init(argc, argv, "location_monitor");
+//   ros::NodeHandle nh;
+
+//   //instance of the class LandmarkMontior
+//   LandmarkMonitor monitor;
+//   //create services and hook them up with the callback function defined above as method of the class
+//   ros::ServiceServer get_closest = nh.advertiseService("get_closest", &LandmarkMonitor::GetClosest_callback, &monitor);
+//   ros::ServiceServer get_distance = nh.advertiseService("get_distance", &LandmarkMonitor::GetDistance_callback, &monitor);
+
+//   //create subscriber and hool them with the callback funtion defined above as method od the class
+// //   ros::Subscriber sub = nh.subscribe("odom",1, &LandmarkMonitor::Odom_callback, &monitor);
+//   ros::spin();
+//   return 0;
+// }
+
+#include<string>
+#include<math.h>
+#include<map>
+
+#include "ros/ros.h"
+#include "tbot3_msgs/GetClosest.h"
+#include "tbot3_msgs/GetDistance.h"
+#include "nav_msgs/Odometry.h"
+
+
+struct Landmark
+{
+    /* attributes of landmark list */
+    std::string name;  // name of landmark, x & y position
+    double x;          
+    double y;
+};
+
+class LandmarkMonitor
+{
+private:
+    /* attributes of the class*/
+    std::map<std::string, Landmark> landmark_list;
+    double robot_pos_x;    // get these values by listening odom topic
+    double robot_pos_y;    // for listening subscribe to the odom topic via subscribe functionality
+
+public:
+    LandmarkMonitor();
+    ~LandmarkMonitor();
+
+    /* class methods to implement service callback functions*/
+    bool GetClosest_callback(tbot3_msgs::GetClosest::Request& request, tbot3_msgs::GetClosest::Response& response){
+        ROS_INFO("GetClosest Service Called");
+
+        double close_distance = -1;
+        std::string close_landmark("");
+
+        for(const auto& x: landmark_list){
+            const Landmark& landmark = x.second;
+            // get position of the landmark
+            double landmark_pos_x = landmark.x;
+            double landmark_pos_y = landmark.y;
+
+            //compute robot distance from landmark
+            double dx = landmark_pos_x - robot_pos_x;
+            double dy = landmark_pos_y - robot_pos_y;
+
+            // euclidian distance
+            double dd = sqrt(dx*dx + dy*dy);
+
+            if(close_distance == -1 || dd< close_distance){
+                close_distance = dd;
+                close_landmark = x.first;
+            }
+        }
+
+        response.name = close_landmark;
+        return true;
+    }
+
+    bool GetDistance_callback(tbot3_msgs::GetDistance::Request& request, tbot3_msgs::GetDistance::Response& response){
+        ROS_INFO("GetDistance Service Called for [%s]", request.name.c_str());
+
+        //check landmark exit or not
+        if(landmark_list.find(request.name) == landmark_list.end()){
+            ROS_ERROR("Unknown Landmark [%s]", request.name.c_str());
+            return false;
+        }
+        //if exit  get landmark_x_pos, landmark_y_pos
+        const Landmark &landmark_variable = landmark_list[request.name];
+        double landmark_x_pos = landmark_variable.x;
+        double landmark_y_pos = landmark_variable.y;
+
+        // compute robot distance from the landmark positions
+        double dx = landmark_x_pos - robot_pos_x;
+        double dy = landmark_y_pos - robot_pos_y;
+
+        //return the response in the form of euclidian distance
+        response.distance = sqrt(dx*dx + dy*dy);
+        return true; // to tell that call has been sucessful
+    }
+
+
+    /* class methods to implemet subscribe callbcak function*/
+    void Odom_callback(const nav_msgs::OdometryConstPtr& msg){
+        robot_pos_x = msg->pose.pose.position.x;
+        robot_pos_y = msg->pose.pose.position.y;
+    }
+
+};
+
+LandmarkMonitor::LandmarkMonitor()
+    :landmark_list(), robot_pos_x{0}, robot_pos_y{0}{
+    landmark_list = {
+        {"Construction_barrel", {"Constrcution_barrel", 5.20314, 3.591}},
+        {"Dumpster", {"Dumpster", 3.190, -4.9259}},
+        {"Jersey_barrier", {"Jersey_barrier", 0.31978, 1.38024}},
+        {"Number1", {"Number1", 0.359466, 3.82409}}
+    };
+}
+
+LandmarkMonitor::~LandmarkMonitor()
+{
+}
+
+int main(int argc, char**argv){
+    //initialize the ros node with name "landmark_monitor"
+    ros::init(argc, argv, "landmark_monitor");
+    ros::NodeHandle nh;
+
+    //instance of the class
+    LandmarkMonitor monitor;
+
+    //create ros services and hook them up with above defined service callback functions
+    ros::ServiceServer get_closest = nh.advertiseService("get_closest", &LandmarkMonitor::GetClosest_callback, &monitor);
+    ros::ServiceServer get_distance = nh.advertiseService("get_distance", &LandmarkMonitor::GetDistance_callback, &monitor);
+
+    //create subscriber and hook them up with above defined subscribe callback function
+    ros::Subscriber sub = nh.subscribe("odom",1, &LandmarkMonitor::Odom_callback,&monitor);
+
+    ros::spin();
+    return 0;
+}
+
+
+/*
+##  callback functions are the one that will implement the service
+##  why using class :
+    if there is a state that is shared btw main and callback function, 
+    for this that state has to be the global variable, to avoid this thing,
+    we are implementing the service callback functions as method of the class
+
+
+-------------------ROS CLIENT---------------
+## Client is the one who is asking for the services provided by the Node
+*/ 
